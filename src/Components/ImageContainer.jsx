9c7de10c5c6f0dfc453ce6b73d9422e115e6b756
som-syom <dhyeon@student.42seoul.kr>
2022-02-02 17:52:01 +0900

    feat: onClick 함수 만들어 자식에 전달

--- src/Components/ImageContainer.jsx
+++ src/Components/ImageContainer.jsx
@@ -6,6 +6,7 @@ import "Components/scss/ImageContainer.scss";
 function ImageContainer() {
   const [itemData, setItemData] = useState([]);
   const [mainImage, setMainImage] = useState();
+  const [activeItem, setActiveItem] = useState("");
 
   useEffect(() => {
     const getItemData = async () => {
@@ -18,13 +19,30 @@ function ImageContainer() {
     getItemData();
   }, []);
 
+  const onClickItem = (e, id) => {
+    e.preventDefault();
+
+    if (id === activeItem) setActiveItem("");
+    else setActiveItem(id);
+  };
+
+  const onClickImage = (e) => {
+    e.preventDefault();
+    setActiveItem("");
+  };
+
   return (
     <div className="image__container">
-      <img src={mainImage} alt="room image" />
+      <img src={mainImage} alt="room image" onClick={onClickImage} />
       {itemData.length > 0 &&
-        itemData.map((item, index) => {
+        itemData.map((item) => {
           return (
-            <MagnifierButton item={item} index={index} key={item.productId} />
+            <MagnifierButton
+              item={item}
+              key={item.productId}
+              onClickItem={onClickItem}
+              isActive={activeItem === item.productId ? true : false}
+            />
           );
         })}
     </div>
