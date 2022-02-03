2a7b281c2fd33c20a9003c6baec7afd833ecbc95
som-syom <dhyeon@student.42seoul.kr>
2022-02-03 17:07:31 +0900

    feat: mouse event 추가

--- src/Components/Swiper.jsx
+++ src/Components/Swiper.jsx
@@ -1,17 +1,64 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import SwiperItem from "Components/SwiperItem";
 import "Components/scss/Swiper.scss";
 
 function Swiper({ item, activeItem, onClickItem, currentX = 0 }) {
   const swiperRef = useRef();
+  const sliderRef = useRef();
+  const [isDrag, setIsDrag] = useState(false);
+  const [startX, setStartX] = useState(0);
 
   useEffect(() => {
     swiperRef.current.style.transitionDuration = "300ms";
     swiperRef.current.style.transform = `translateX(${currentX}px)`;
   }, [currentX, activeItem]);
 
+  const onDragStart = (e) => {
+    e.preventDefault();
+    setIsDrag(true);
+    setStartX(e.pageX + sliderRef.current.scrollLeft);
+    // setStartX(currentX);
+  };
+
+  const onDragEnd = () => {
+    setIsDrag(false);
+  };
+
+  const onDragMove = (e) => {
+    if (isDrag) {
+      const { scrollLeft } = sliderRef.current;
+      sliderRef.current.scrollLeft = startX - e.pageX;
+      if (scrollLeft === 0) {
+        setStartX(e.pageX);
+      }
+    }
+  };
+
+  const delay = 10;
+  const throttle = (func, ms) => {
+    let throttled = false;
+    return (...args) => {
+      if (!throttled) {
+        throttled = true;
+        setTimeout(() => {
+          func(...args);
+          throttled = false;
+        }, ms);
+      }
+    };
+  };
+
+  const onThrottleDragMove = throttle(onDragMove, delay);
+
   return (
-    <div className="swiper__container">
+    <div
+      className="swiper__container"
+      onMouseDown={onDragStart}
+      onMouseMove={isDrag ? onThrottleDragMove : null}
+      onMouseUp={onDragEnd}
+      onMouseLeave={onDragEnd}
+      ref={sliderRef}
+    >
       <div className="swiper__wrapper" ref={swiperRef}>
         {item &&
           item.map((el, idx) => {
@@ -21,6 +68,7 @@ function Swiper({ item, activeItem, onClickItem, currentX = 0 }) {
                 idx={idx}
                 activeItem={activeItem}
                 onClickItem={onClickItem}
+                key={el.productId}
               />
             );
           })}
